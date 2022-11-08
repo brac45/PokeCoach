047b40efe6f10a6cfb6418760e48ebbb5c210903
Jong Ho Lee <mikejhlee04@gmail.com>
2022-11-07 21:29:40 -0500

    Initial Commit

---
+++ PokeCoach/PokeCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeCoachApp.swift
+//  PokeCoach
+//
+//  Created by Jong Ho Lee on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
