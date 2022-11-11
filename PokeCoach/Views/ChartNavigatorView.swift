bbec42cee8c1c819ea80072bc22512ee24881ef7
Jong Ho Lee <mikejhlee04@gmail.com>
2022-11-11 16:11:45 -0500

    Initial commit

---
+++ PokeCoach/Views/ChartNavigatorView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartNavigatorView.swift
+//  PokeCoach
+//
+//  Created by Jong Ho Lee on 11/11/22.
+//
+
+import SwiftUI
+
+struct ChartNavigatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartNavigatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartNavigatorView()
+    }
+}
---
+++ PokeCoach/Views/HeartrateView.swift
@@ -0,0 +1,31 @@
+//
+//  HeartrateView.swift
+//  PokeCoach
+//
+//  Created by Jong Ho Lee on 11/7/22.
+//
+
+import SwiftUI
+import Charts
+
+struct HeartrateView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct HeartrateViewChart {
+    var body: some View {
+        Chart {
+            
+        }
+    }
+}
+
+struct HeartrateView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeartrateView()
+    }
+}
