4ae29a11ac0f2d724fd53b462d5f042b10d39700
Jong Ho Lee <mikejhlee04@gmail.com>
2022-11-30 10:59:18 -0500

    Improvements on audio summary

--- PokeCoach/Views/HeartrateView.swift
+++ PokeCoach/Views/HeartrateView.swift
@@ -10,6 +10,10 @@ import Charts
 import CoreHaptics
 import AVFoundation
 
+enum PlayAudioState {
+    case initial, setting, ready
+}
+
 struct HeartrateView: View {
     @ObservedObject var hrData: HeartrateModelData
     @ObservedObject var hapticManager: HapticsManager
@@ -23,8 +27,17 @@ struct HeartrateView: View {
     
     @State private var speechSynthesizer = AVSpeechSynthesizer()
     
+    @State private var audioState: PlayAudioState = PlayAudioState.initial
+    
     func playSynthesizedSummary() {
+        if audioState != PlayAudioState.ready {
+            print("Audio is not ready")
+            audioState = PlayAudioState.initial
+            return
+        }
+        
         print("Current Min: \(minIdxForSpeech), max: \(maxIdxForSpeech)")
+        
         if minIdxForSpeech < maxIdxForSpeech {
             if minHr >= 9999 || maxHr < 0 {
                 return
@@ -39,49 +52,64 @@ struct HeartrateView: View {
             let rangedMeanHr = Double(sum) / Double(count)
             let relativeChange = Int(((rangedMeanHr - hrData.meanHr) / hrData.meanHr) * 100)
             
-            // TODO: Build string here
             let summary = """
-                    From the range you explored,
+                    From the range you indicated,
                     your minimum heart rate is \(minHr) beats per minute,
                     and your maximum heart rate is \(maxHr) beats per minute.
-                    Your averate heart rate in this range is \(Int(rangedMeanHr)) beats per minute.
-                    This is a \(relativeChange) percent change from the overall average of your run, where
-                    your overall average was \(Int(hrData.meanHr))
+                    Your averate heart rate in this range is \(Int(rangedMeanHr)) beats per minute,
+                    which is a \(relativeChange) percent change from the overall average of your run. Your overall average was \(Int(hrData.meanHr))
                     """
             
             let utterance = AVSpeechUtterance(string: summary)
             utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
             
             print("Playing \(summary)")
-            //let synthesizer = AVSpeechSynthesizer()
             speechSynthesizer.speak(utterance)
         }
+        
+        audioState = PlayAudioState.initial
+    }
+    
+    func setFirstPointForAudio() {
+        minIdxForSpeech = chartIdx
+        audioState = PlayAudioState.setting
+    }
+    
+    func setSecondPointForAudio() {
+        // Check if they should be swapped
+        if chartIdx < minIdxForSpeech {
+            maxIdxForSpeech = minIdxForSpeech
+            minIdxForSpeech = chartIdx
+        } else if chartIdx > minIdxForSpeech {
+            maxIdxForSpeech = chartIdx
+        } else {
+            // Should send a toast message or something
+            audioState = PlayAudioState.initial
+            return
+        }
+        
+        // Find the y min and y max
+        minHr = 9999
+        maxHr = -1
+        for i in Int(minIdxForSpeech)...Int(maxIdxForSpeech) {
+            let val = hrData.datapoints[i].hr
+            
+            if val < minHr {
+                minHr = val
+            }
+            if val > maxHr {
+                maxHr = val
+            }
+        }
+        
+        audioState = PlayAudioState.ready
     }
     
     func onIdxChange() {
         print("Idx change: \(chartIdx)")
         
         if hapticManager.doHapticFeedback {
-            if chartIdx < minIdxForSpeech {
-                print("SET min: \(minIdxForSpeech)")
-                minIdxForSpeech = chartIdx
-            }
-            if chartIdx > maxIdxForSpeech {
-                print("SET max: \(maxIdxForSpeech)")
-                maxIdxForSpeech = chartIdx
-            }
-            
-            if hrData.datapoints[Int(chartIdx)].hr < minHr {
-                print("SET minhr: \(minHr)")
-                minHr = hrData.datapoints[Int(chartIdx)].hr
-            }
-            if hrData.datapoints[Int(chartIdx)].hr > maxHr {
-                print("SET maxhr: \(maxHr)")
-                maxHr = hrData.datapoints[Int(chartIdx)].hr
-            }
-            
             if hapticManager.doTransientInstead {
-                /// TODO: Update frequency here
                 hapticManager.updateTransientHapticInterval(bpm: hrData.datapoints[Int(chartIdx)].hr)
                 hapticManager.playTransientPattern()
             } else {
@@ -91,13 +119,6 @@ struct HeartrateView: View {
                 
                 hapticManager.updateContinuousHapticParameters(refMax: refMax, refMin: refMin, curVal: curVal)
             }
-        } else {
-            minIdxForSpeech = Double.infinity
-            maxIdxForSpeech = -1.0
-            minHr = 9999
-            maxHr = -1
-            print("RESET min: \(minIdxForSpeech), max: \(maxIdxForSpeech)")
-            print("RESET minHr: \(minHr), maxHr: \(maxHr)")
         }
     }
     
@@ -137,13 +158,20 @@ struct HeartrateView: View {
                     }
                 
             }
+            
             HStack {
                 Spacer(minLength: 20)
                 Label("Time: \(dateToTimeString())", systemImage: "stopwatch")
                 Spacer(minLength: 20)
                 Label("Heartrate: \(hrData.datapoints[Int(chartIdx)].hr)", systemImage: "heart")
                 Spacer()
-                Button("Play Audio Summary", action: playSynthesizedSummary)
+                if self.audioState == PlayAudioState.initial {
+                    Button("Set First Point", action: setFirstPointForAudio)
+                } else if self.audioState == PlayAudioState.setting {
+                    Button("Set Last Point", action: setSecondPointForAudio)
+                } else if self.audioState == PlayAudioState.ready {
+                    Button("Play Audio Summary", action: playSynthesizedSummary)
+                }
                 Toggle(isOn: $hapticManager.doHapticFeedback) {
                     Text("Haptics")
                         .frame(maxWidth: 70, alignment: .trailing)
